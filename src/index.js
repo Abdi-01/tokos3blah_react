c83717f9131d4b213ccfa81a6a543e1b21165f4c
dickyadi <dickysusiloa@gmail.com>
2021-10-06 12:44:12 +0700

    lalala

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom'
+// redux configuration
+import { Provider } from 'react-redux'; // untuk menghungkan antara action, reducer dan component
+import { createStore } from 'redux'; // untuk membuat global store untuk reducer
+import { Reducers } from './reducers'
+
+const storeReducer = createStore(Reducers)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={storeReducer}>
+    {/* // BrwoserRouter : penghubung seluruh page yg di akomodir oleh App.js */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
