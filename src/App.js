1ff0c8036bc3b013efb5ac4f3f4b9bfb893ebef6
dickyadi <dickysusiloa@gmail.com>
2021-10-09 12:19:03 +0700

    FE-01 Fix login register

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
-import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-// import { Navbar } from "./components/Navbar";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import Admin from "./pages/Admin";
-import Userprofile from "./pages/Userprofile";
-// import Home from "./pages/Home";
+import logo from './logo.svg';
 import './App.css';
-import LandingPage from './pages/landingPage' //import pages 
-import Navbar from './components/navbar'
-// import DetailPage from './pages/detailAlbum';
-// import LoginPage from './pages/loginPage';
-// import VerificationPage from './pages/verification';
-// import UserList from './pages/registerPage';
 
-export class App extends Component {
-  render() {
-    return (
-      <>
-      <BrowserRouter>
-        <Navbar />
-          <Switch>
-            {/* <Route path="/" exact component={Home} /> */}
-            <Route path="/" component={LandingPage} exact />
-            <Route path="/admin" component={Admin} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/user" component={Userprofile} />
-      {/* <Route path="/authentication/:token" component={VerificationPage} /> */}
-      {/* <Route path="/user-list" component={UserList} /> */}
-      {/* <Route path="/account" component={LoginPage} /> */}
-      {/* <Route path="/detail-album" component={DetailPage} /> */}
-          </Switch>
-        </BrowserRouter>
-      </>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
