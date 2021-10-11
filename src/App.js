67216c677942e1de27109f4bb6d8cec96a165cad
dickyadi <dickysusiloa@gmail.com>
2021-10-11 17:39:04 +0700

    FE-Dicky-TOK-6 admin only access by admin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserHistory } from "history";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 
-import Navbar  from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Verification from "./pages/auth/Verification";
@@ -11,21 +13,25 @@ import Admin from "./pages/Admin";
 import Userprofile from "./pages/Userprofile";
 import Home from "./pages/Home";
 
-export class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <Route path="/" exact component={Home} />
-        <Route path="/admin" component={Admin} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/authentication/:token" component={Verification} />
-        <Route path="/user" component={Userprofile} />
-      </div>
-    );
-  }
+export const history = createBrowserHistory();
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<Router history={history}>
+					<Navbar />
+					<Switch>
+						<Route path="/" exact component={Home} />
+						<Route path="/admin" component={Admin} />
+						<Route path="/login" component={Login} />
+						<Route path="/register" component={Register} />
+						<Route path="/authentication/:token" component={Verification} />
+						<Route path="/user" component={Userprofile} />
+					</Switch>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -6,90 +6,93 @@ import { authLogin } from "../../actions/login";
 import { connect } from "react-redux";
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      alertShow: "none",
-      redirect: false,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			alertShow: "none",
+			redirect: false,
+		};
+	}
 
-  onBtLogin = () => {
-    axios.post(URL_API + `/users/login`, {
-        email: this.inEmail.value,
-        password: this.inPass.value
-      })
-          .then((res) => {
-            console.log(res)
-        localStorage.setItem("token_s3blah", res.data.token);
-        // menjalankan fungsi action
-        this.props.authLogin(res.data.dataLogin);
-        console.log(res.data.token);
-        console.log(res.data.dataLogin);
-        this.setState({ redirect: true });
-        this.setState({ logout: true });
-        console.log("Login Success ✔");
-      })
-         .catch((err) => console.log(err));
-  };
+	onBtLogin = () => {
+		axios
+			.post(URL_API + `/users/login`, {
+				email: this.inEmail.value,
+				password: this.inPass.value,
+			})
+			.then((res) => {
+				sessionStorage.setItem("role", res.data.dataLogin.role);
 
-  render() {
-      if (this.state.redirect) {
-        console.log("Redirect");
-        return <Redirect to="/" />;
-      }
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-12 text-center">
-            <h1>Login now!</h1>
-            <p className="lead">
-              Login now and start shopping in the most affordable ecommerce
-              platform
-            </p>
-          </div>
-        </div>
-        <div className="row mt-5">
-          <div className="col-4 offset-4">
-            {/* {
+				console.log(res);
+				localStorage.setItem("token_s3blah", res.data.token);
+				// menjalankan fungsi action
+				this.props.authLogin(res.data.dataLogin);
+				console.log(res.data.token);
+				console.log(res.data.dataLogin);
+				this.setState({ redirect: true });
+				this.setState({ logout: true });
+				console.log("Login Success ✔");
+			})
+			.catch((err) => console.log(err));
+	};
+
+	render() {
+		if (this.state.redirect) {
+			console.log("Redirect");
+			return <Redirect to="/" />;
+		}
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-12 text-center">
+						<h1>Login now!</h1>
+						<p className="lead">
+							Login now and start shopping in the most affordable ecommerce
+							platform
+						</p>
+					</div>
+				</div>
+				<div className="row mt-5">
+					<div className="col-4 offset-4">
+						{/* {
               this.props.userGlobal.errMsg ?
               <div className="alert alert-danger">{this.props.userGlobal.errMsg}</div>
               : null
             } */}
-            <div className="card">
-              <div className="card-body">
-                <h5 className="font-weight-bold mb-3">Login</h5>
-                <input
-                  name="Email"
-                  ref={(el) => (this.inEmail = el)}
-                  placeholder="Email"
-                  type="email"
-                  className="form-control my-2"
-                />
-                <input
-                  name="password"
-                  ref={(el) => (this.inPass = el)}
-                  placeholder="Password"
-                  type="password"
-                  className="form-control my-2"
-                />
-                <div className="d-flex flex-row justify-content-between align-items-center">
-                  <button
-                    onClick={this.onBtLogin}
-                    className="btn btn-primary mt-2"
-                  >
-                    Login
-                  </button>
-                  <Link to="/register">Or Register</Link>
-                  <Link to="/">Forgot Password</Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+						<div className="card">
+							<div className="card-body">
+								<h5 className="font-weight-bold mb-3">Login</h5>
+								<input
+									name="Email"
+									ref={(el) => (this.inEmail = el)}
+									placeholder="Email"
+									type="email"
+									className="form-control my-2"
+								/>
+								<input
+									name="password"
+									ref={(el) => (this.inPass = el)}
+									placeholder="Password"
+									type="password"
+									className="form-control my-2"
+								/>
+								<div className="d-flex flex-row justify-content-between align-items-center">
+									<button type="button"
+										onClick={this.onBtLogin }
+										className="btn btn-primary mt-2"
+									>
+										Login
+									</button>
+									<Link to="/register">Or Register</Link>
+									<Link to="/">Forgot Password</Link>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default connect(null, { authLogin })(Login);
