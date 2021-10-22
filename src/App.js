eb3b2e26c3b88d5eecc44470273eec0e46149114
derimulia <derimulia95@gmail.com>
2021-10-22 22:40:15 +0700

    FE DERI ADD HOME ADD FORM ALAMAT

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { createBrowserHistory } from "history";
 
 import { Router, Route, Switch } from "react-router-dom";
 
-import Navbar  from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Verification from "./pages/auth/Verification";
@@ -13,38 +13,43 @@ import Admin from "./pages/Admin";
 import SuperAdmin from "./pages/SuperAdmin";
 import Userprofile from "./pages/Userprofile";
 import Home from "./pages/Home";
-import Product from "./pages/Product.jsx"
-import Cart from "./pages/Cart.jsx"
+import Product from "./pages/Product.jsx";
+import Cart from "./pages/Cart.jsx";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import ChangePassword from "./pages/auth/ChangePassword";
 import AddAdmin from "./pages/AddAdmin";
 import AddWarehouse from "./pages/AddWarehouse";
+import Form_user from "./components/Form_user";
+import ProductDetail from "./pages/ProductDetail";
+
 export const history = createBrowserHistory();
 class App extends React.Component {
-	render() {
-		return (
-			<div className="App">
-				<Router history={history}>
-					<Navbar />
-					<Switch>
-						<Route path="/" exact component={Home} />
-						<Route path="/admin" component={Admin} />
-						<Route path="/super_admin" component={SuperAdmin} />
-						<Route path="/add_admin" component={AddAdmin} />
-						<Route path="/add_warehouse" component={AddWarehouse} />
-						<Route path="/login" component={Login} />
-						<Route path="/register" component={Register} />
-						<Route path="/authentication/:token" component={Verification} />
-						<Route path="/user" component={Userprofile} />
-						<Route path="/product" component={Product} />
-						<Route path="/cart" component={Cart} />
-						<Route path="/ForgotPassword" component={ForgotPassword} />
-						<Route path="/change-password/:token" component={ChangePassword} />
-					</Switch>
-				</Router>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <Router history={history}>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/admin" component={Admin} />
+            <Route path="/super_admin" component={SuperAdmin} />
+            <Route path="/add_admin" component={AddAdmin} />
+            <Route path="/add_warehouse" component={AddWarehouse} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/authentication/:token" component={Verification} />
+            <Route path="/user" component={Userprofile} />
+            <Route path="/product" component={Product} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/ForgotPassword" component={ForgotPassword} />
+            <Route path="/change-password/:token" component={ChangePassword} />
+            <Route path="/form_user" component={Form_user} />
+            <Route path="/ProductDetail/:productId" component={ProductDetail} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/Userprofile.js
+++ src/pages/Userprofile.js
@@ -1,32 +1,59 @@
 import React, { Component } from "react";
-import Form_user from "../components/Form_user";
 import User_data from "../components/User_data";
+import Form_alamat from "../components/Form_alamat";
 
 export class Userprofile extends Component {
   render() {
     return (
-      <div className="container mt-5">
-        <div className="card text-center">
-          <div className="card-header">
-            <ul className="nav nav-tabs card-header-tabs">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">
-                  User Profile
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-dark" href="#">
-                  Alamat
-                </a>
-              </li>
-            </ul>
+      <div className="container mt-3">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <a
+              class="nav-link active"
+              id="home-tab"
+              data-toggle="tab"
+              href="#home"
+              role="tab"
+              aria-controls="home"
+              aria-selected="true"
+            >
+              Profile
+            </a>
+          </li>
+          <li class="nav-item" role="presentation">
+            <a
+              class="nav-link"
+              id="alamat-tab"
+              data-toggle="tab"
+              href="#alamat"
+              role="tab"
+              aria-controls="alamat"
+              aria-selected="false"
+            >
+              Alamat
+            </a>
+          </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+          <div
+            class="tab-pane fade show active"
+            id="home"
+            role="tabpanel"
+            aria-labelledby="home-tab"
+          >
+            <div className="container mt-3">
+              <User_data />
+            </div>
           </div>
-          <div className="card-body">
-            <div className="row">
-              <div className="col-lg">
-                <Form_user />
-                {/* <User_data /> */}
-              </div>
+
+          <div
+            class="tab-pane fade"
+            id="alamat"
+            role="tabpanel"
+            aria-labelledby="alamat-tab"
+          >
+            <div className="container mt-3">
+              <Form_alamat />
             </div>
           </div>
         </div>
